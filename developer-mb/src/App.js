d146d8d48db91b0cf23f5d1f984da8280d092a1c
Mohammad Jaiteh <jaitehmohammad.com>
2019-09-30 16:40:58 -0400

    preparation

--- developer-mb/src/App.js
+++ developer-mb/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      check: '',
+      check1: '',
+      check2: ''
+    }
+    let check = axios.get('https://programming-quotes-api.herokuapp.com/quotes/random')
+    console.log(check);
+    let check1 = axios.get('http://numbersapi.com/4')
+    console.log(check1);
+    let check2 = axios.get('https://official-joke-api.appspot.com/jokes/random')
+    console.log(check2);
+  }
+  render(){
+    return (
+      <div></div>
+    )
+  }
+
 }
+
+    
+
+
 
 export default App;
