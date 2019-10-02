5214d02f3d1d842a606e2d1872039ed2fff20ac1
Mohammad Jaiteh <jaitehmohammad.com>
2019-10-02 14:01:46 -0400

    number Api finish

--- developer-mb/src/components/Numbers.js
+++ developer-mb/src/components/Numbers.js
@@ -5,24 +5,53 @@ class Numbers extends Component {
     constructor(props) {
         super(props)
         this.state = ({
-            number: [] 
+            number: [],
+            value: '',
+            numberPick: 0
         })
     }
-    async componentDidMount() {
-        const numb = 9
+    // async componentDidMount() {
+    //     const numb = ''
+    //     let number = await axios.get(`http://numbersapi.com/${numb}?json`)
+    //     console.log(number)
+    //     this.setState({
+    //       number: number.data
+    //     })
+    //     console.log(number) 
+    //     console.log(this.state.number)
+    // }
+
+    handleNumInput = (event) => {
+        this.setState({
+            value: parseInt(event.target.value)
+        })
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        const numb = this.state.value
         let number = await axios.get(`http://numbersapi.com/${numb}?json`)
         console.log(number)
         this.setState({
-          number: number.data
+            number: number.data
         })
-        console.log(number); 
         console.log(this.state.number)
-      }
+        console.log(this.state.value)  
+
+    }
 
     render () {
         return (
             <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Type any number:
+                        <input type="number" name="number" value={this.state.value} onChange={this.handleNumInput}/>
+                    </label>
+                    <input type="submit" value="submit"/>
+                </form>
                 {this.state.number.text}
+
             </div>
         )
     }
